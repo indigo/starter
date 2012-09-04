1a9d9e7a2c45d6154c91030dbc954344c2723b0f
Ivan Mansueto <mansiva2000@gmail.com>
2012-09-04 17:03:08 -0400

    Some optimization
    Removed games field from users

--- dev/server/main.py
+++ dev/server/main.py
@@ -12,15 +12,19 @@ jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.di
 
 
 class User(db.Model):
-  game_keys = db.ListProperty(db.Key, default=None)
+  #games = db.ListProperty(db.Key, default=None)
+
+  @property
+  def games(self):
+      return Game.gql("WHERE users = :1", self.key())
 
 class Game(db.Model):
   type = db.IntegerProperty()
   open = db.BooleanProperty()
-  user_keys = db.ListProperty(db.Key, default=None)
+  users = db.ListProperty(db.Key, default=None)
 
 class Move(db.Model):
-  user_key = db.ReferenceProperty(User)
+  user = db.ReferenceProperty(User)
   data = db.ListProperty(int)
 
 
@@ -29,23 +33,29 @@ class AdminPage(webapp2.RequestHandler):
   def get(self):
     user = user_games = game = game_moves = None
 
+    # Create new user
     if self.request.get('new_user'):
       User().put()
 
+    # Fetch user's games
     if self.request.get('user_key'):
       try:
         user = db.get(self.request.get('user_key'))
-        user_games = db.get(user.game_keys)
+        user_games = user.games #db.get(user.games)
       except:
         self.response.out.write("Invalid user_key")
+        return
 
+    # Fetch game's moves
     if self.request.get('game_key'):
       try:
         game = db.get(self.request.get('game_key'))
         game_moves = db.Query(Move).ancestor(game.key())
       except:
         self.response.out.write("Invalid game_key")
+        return
 
+    # Load page
     template_values = { 'all_users': User.all(), 'user': user, 'user_games': user_games, 'game': game, 'game_moves': game_moves }
     template = jinja_environment.get_template('admin.html')
     self.response.out.write(template.render(template_values))
@@ -55,22 +65,27 @@ class UserPage(webapp2.RequestHandler):
 
   def get(self):
 
+    # Get user from key
     try:
       user = db.get(self.request.get('user_key'))
+
+    # Create new user
     except:
       user = User()
       user.put()
 
-    all_games = db.get(user.game_keys)
+    # Sort games based on turn
+    all_games = user.games #db.get(user.games)
     waiting_games = []
     myturn_games = []
 
     for game in all_games:
-      if not game.open and game.user_keys[0] == user.key():
+      if not game.open and game.users[0] == user.key():
         myturn_games.append(game)
       else:
         waiting_games.append(game)
 
+    # Load page
     template_values = { 'user': user, 'myturn_games': myturn_games, 'waiting_games': waiting_games }
     template = jinja_environment.get_template('user.html')
     self.response.out.write(template.render(template_values))
@@ -91,10 +106,10 @@ class GamePage(webapp2.RequestHandler):
       new_game = True
 
       # Find next active game
-      for game_key in user.game_keys:
-        user_game = db.get(game_key)
+      for user_game in user.games:
+      #  user_game = db.get(game_key)
 
-        if not user_game.open and user_game.user_keys[0] == user.key():
+        if not user_game.open and user_game.users[0] == user.key():
           game = user_game
           new_game = False
           break
@@ -103,7 +118,7 @@ class GamePage(webapp2.RequestHandler):
       if not game:
         open_games = Game.gql("WHERE open=True")
         for open_game in open_games:
-          if open_game.user_keys[0] != user.key():
+          if open_game.users[0] != user.key():
             game = open_game
             break
 
@@ -114,14 +129,12 @@ class GamePage(webapp2.RequestHandler):
       # Add game to user
       if new_game:
         game.open = False
-        game.user_keys.insert(0, user.key())
+        game.users.insert(0, user.key())
         game.put()
-
-        user.game_keys.append(game.key())
-        user.put()
 
     game_moves = db.Query(Move).ancestor(game.key())
 
+    # Load page
     template_values = { 'user': user, 'game': game, 'game_moves': game_moves }
     template = jinja_environment.get_template('game.html')
     self.response.out.write(template.render(template_values))
@@ -135,17 +148,23 @@ class PlayPage(webapp2.RequestHandler):
     game = db.get(self.request.get('game_key'))
     data = eval(self.request.get('game_data'))
 
-    move = Move(parent=game.key(), user_key=user.key(), data=data)
+    # Create move
+    move = Move(parent=game.key(), user=user.key(), data=data)
     move.put()
 
-    if len(game.user_keys) == 1:
+    # Set game as open
+    if len(game.users) == 1:  # TODO: replace 1 with dynamic value based on game type
       game.open = True
+
+    # Move current user to end of list
     else:
-      game.user_keys.append(game.user_keys.pop(0))
+      game.users.append(game.users.pop(0))
+
     game.put()
 
     game_moves = db.Query(Move).ancestor(game.key())
 
+    # Load page
     template_values = { 'user': user, 'game': game, 'game_moves': game_moves }
     template = jinja_environment.get_template('game.html')
     self.response.out.write(template.render(template_values))
